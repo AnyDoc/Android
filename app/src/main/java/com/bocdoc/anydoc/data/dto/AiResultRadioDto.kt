75a6fa3b9987c725761600fe9a37ca835f309802
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-06-17 00:27:35 +0900

    [FIX] : ai_result 라디오 버튼 클릭 가능하도록 수정 (#10)

---
+++ app/src/main/java/com/bocdoc/anydoc/data/dto/AiResultRadioDto.kt
@@ -0,0 +1,6 @@
+package com.bocdoc.anydoc.data.dto
+
+data class AiResultRadioDto(
+    val title: String,
+    var check: Boolean,
+)
--- app/src/main/java/com/bocdoc/anydoc/presentation/viewholder/AiResultRadioViewHolder.kt
+++ app/src/main/java/com/bocdoc/anydoc/presentation/viewholder/AiResultRadioViewHolder.kt
@@ -1,17 +1,14 @@
 package com.bocdoc.anydoc.presentation.viewholder
 
-import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
-import coil.transform.RoundedCornersTransformation
-import com.bocdoc.anydoc.R
-import com.bocdoc.anydoc.databinding.ItemAiResultImgBinding
+import com.bocdoc.anydoc.data.dto.AiResultRadioDto
 import com.bocdoc.anydoc.databinding.ItemAiResultRadioBinding
 
 class AiResultRadioViewHolder(private val binding: ItemAiResultRadioBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(radioTitle: String) {
-        binding.tvAiResultRadioTitle.text = radioTitle
+    fun bind(aiRadio : AiResultRadioDto) {
+        binding.tvAiResultRadioTitle.text = aiRadio.title
+        binding.tvAiResultRadioTitle.isSelected = aiRadio.check
     }
 }
